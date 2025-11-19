cde10b7e8c9d4b6e93cc7665159d87b3499a7745
am-lozano <am.lozano@iesnervion.es>
2025-11-19 14:30:37 +0100

    corregir

---
+++ Tema10/CompositionRoot/InversifyConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace CompositionRoot
+{
+    internal class InversifyConfig
+    {
+
+        public static IServiceCollecion AddCompositionRoot(CallConvThiscall IServiceCollection services, IConfiguration configuration)
+        {
+            
+        }
+
+    }
+}
