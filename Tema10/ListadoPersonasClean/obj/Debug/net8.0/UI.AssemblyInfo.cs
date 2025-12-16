70b164a4390ab643c3f90ff7496cd9fadb31bfdb
am-lozano <am.lozano@iesnervion.es>
2025-12-16 14:05:12 +0100

    .

--- Tema10/ListadoPersonasClean/obj/Debug/net8.0/UI.AssemblyInfo.cs
+++ Tema10/ListadoPersonasClean/obj/Debug/net8.0/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4209cc7d4d2e295f1b88d34eee3c7bbddae8900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22fbaf881328d94ed3691e63d2a487e2fbae144f")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
